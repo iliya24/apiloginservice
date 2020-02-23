a6f12bfa1dd8dd02cb7a5f8eddeeda317186d8d1
iliya <i@i.com>
2020-02-23 18:15:07 +0200

    comit 3

--- controllers/users.js
+++ controllers/users.js
@@ -19,16 +19,17 @@ module.exports={
 
 signUp: async (req,res,next)=>
     {
-        console.log('users signUp controller method');
-        console.log(req.body);
+        res.send("result.user");
+        // console.log('users signUp controller method');
+        // console.log(req.body);
        
-        auth.createUserWithEmailAndPassword(req.body.email,req.body.password).then(
-            result=>{
-                console.log('user:'+result.user);
-            res.send(result.user);
+        // auth.createUserWithEmailAndPassword(req.body.email,req.body.password).then(
+        //     result=>{
+        //         console.log('user:'+result.user);
+        //     res.send(result.user);
 
-            }
-        ); 
+        //     }
+        // ); 
         
 },
 
